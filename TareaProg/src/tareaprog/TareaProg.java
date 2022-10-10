0afd8d2eec1997062a621d7eb1ac36b836c23cee
CataBravoH <cbravo2019@udec.cl>
2022-10-09 22:05:15 -0300

    Clase direccion completa

--- TareaProg/src/tareaprog/TareaProg.java
+++ TareaProg/src/tareaprog/TareaProg.java
@@ -62,6 +62,24 @@ class Cliente{
 
 class Direccion{
     private String direccion;
+
+    public Direccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Direccion: " + direccion;
+    }
+    
 }
 
 class DetalleOrden{
