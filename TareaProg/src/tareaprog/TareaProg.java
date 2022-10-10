e16fdfe238add22b4cb97f4d917e4e96e710763b
CataBravoH <cbravo2019@udec.cl>
2022-10-09 22:02:40 -0300

    clase cliente completa

--- TareaProg/src/tareaprog/TareaProg.java
+++ TareaProg/src/tareaprog/TareaProg.java
@@ -14,11 +14,49 @@ class OrdenCompra{
     private String estado;
     
     
+    
 }
 
 class Cliente{
     private String nombre;
     private String rut;
+    private Direccion direccion;
+
+    public Cliente(String nombre, String rut, Direccion direccion) {
+        this.nombre = nombre;
+        this.rut = rut;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + "Rut: " + rut + ", Direccion: " + direccion;
+    }
+    
     
 }
 
@@ -85,13 +123,57 @@ class Factura extends DocTributario{}
 class Pago{
     private float monto;
     private Date fecha;
+
+    public Pago(float monto, Date fecha) {
+        this.monto = monto;
+        this.fecha = fecha;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    @Override
+    public String toString() {
+        return "Pago{" + "monto:" + monto + ", fecha:" + fecha + '}';
+    }
+    
 }
 
-class Efectivo extends Pago{}
+class Efectivo extends Pago{
+    private float vuelto;
+
+    public Efectivo(float monto, Date fecha) {
+        super(monto, fecha);
+        vuelto=0;
+    }
+    public void calcDevolucion(float pago){
+        vuelto = super.getMonto() - pago;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"vuelto: " + vuelto;
+    }
+    
+    
+}
 
 class Transferencia extends Pago{
     private String banco;
     private String numCuenta;
+
+    public Transferencia(String banco, String numCuenta, float monto, Date fecha) {
+        super(monto, fecha);
+        this.banco = banco;
+        this.numCuenta = numCuenta;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ "banco: " + banco + ", numCuenta: " + numCuenta;
+    }
+    
 }
 
 class Tarjeta extends Pago{
