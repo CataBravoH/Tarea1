be6919abab87345bdbc7267a3065d3c371ca9f16
CataBravoH <cbravo2019@udec.cl>
2022-10-09 23:16:34 -0300

    Actualizadas clase de DocTributario

--- TareaProg/src/tareaprog/TareaProg.java
+++ TareaProg/src/tareaprog/TareaProg.java
@@ -132,11 +132,89 @@ class DocTributario{
     private String numero;
     private String rut;
     private Date fecha;
+    private Direccion direccion;
+    private OrdenCompra OC;
+
+    public DocTributario(String numero, String rut, Date fecha, Direccion direccion) {
+        this.numero = numero;
+        this.rut = rut;
+        this.direccion = direccion;
+        if (fecha == null)
+            this.fecha= new Date();
+        else
+            this.fecha = fecha;
+        
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public OrdenCompra getOC() {
+        return OC;
+    }
+
+    public void setOC(OrdenCompra OC) {
+        this.OC = OC;
+    }
+
+    @Override
+    public String toString() {
+        return "numero=" + numero + ", rut=" + rut + ", fecha=" + fecha + ", direccion=" + direccion;
+    }
+    
+    
 }
 
-class Boleta extends DocTributario{}
+class Boleta extends DocTributario{
 
-class Factura extends DocTributario{}
+    public Boleta(String numero, String rut, Date fecha, Direccion direccion) {
+        super(numero, rut, fecha, direccion);
+    }
+    @Override
+    public String toString(){
+        return "Boleta:"+super.toString();
+    }
+}
+
+class Factura extends DocTributario{
+
+    public Factura(String numero, String rut, Date fecha, Direccion direccion) {
+        super(numero, rut, fecha, direccion);
+    }
+    @Override
+    public String toString(){
+        return "Factura: "+super.toString();
+    }
+}
 
 class Pago{
     float monto;
